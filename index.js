857a0f61fb8bda704de5e7588001e2f345f75ba0
Angelvilad <angelvilad@hotmail.com>
2021-03-29 23:00:06 +0200

    Exercise 3.3 completed

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) => {
   response.json(phonebook)
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = phonebook.find(person => person.id === id);
+
+  if(person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+})
+
 app.get('/info', (request, response) => {
   const dateNow = new Date();
 
