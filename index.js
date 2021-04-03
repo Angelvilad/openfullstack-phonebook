29d53ce2ff37981576220ded55c40ab85d7828fe
Angelvilad <angelvilad@hotmail.com>
2021-04-04 00:29:33 +0200

    Added REACT app as static resource

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('showDataPost', function(req, res){
   if(req.method === 'POST') return JSON.stringify(req.body);
